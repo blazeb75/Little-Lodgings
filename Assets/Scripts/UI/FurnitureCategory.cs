37bf3d1e49651acfc03d65e7c68e26f643590d31
Zac <blazeb75@gmail.com>
2020-10-17 17:46:49 +1000

    Constrained scrolling

--- Assets/Scripts/UI/FurnitureCategory.cs
+++ Assets/Scripts/UI/FurnitureCategory.cs
@@ -11,7 +11,7 @@ public class FurnitureCategory : MonoBehaviour
     public string pathOverride = "";
     public GameObject furnitureButtonPrefab;
 
-    private Transform emptyChild;
+    private Transform content;
 
     private string Path
     {
@@ -36,21 +36,23 @@ public class FurnitureCategory : MonoBehaviour
         GameObject[] prefabs = bundle.LoadAllAssets<GameObject>().Where(x => x.name.Contains(label)).ToArray();
         Texture2D[] thumbnails = bundle.LoadAllAssets<Texture2D>().Where(x => x.name.Contains(label)).ToArray();
         bundle.Unload(false);
-        emptyChild = transform.Find("ScrollHolder");
-
-        for (int i = 0; i < prefabs.Length; i++)
+        content = transform.Find("Viewport").Find("Content");
+        int i;
+        for (i = 0; i < prefabs.Length; i++)
         {
             GameObject prefab = prefabs[i];
-            Texture2D thumbnail = thumbnails.Where(x => x.name == prefabs[i].name).First();float offset = -500 + (500 * i);
+            Texture2D thumbnail = thumbnails.Where(x => x.name == prefabs[i].name).First();
+            float offset = -500 + (500 * i);
             CreateFurnitureButton(prefab, Sprite.Create(thumbnail, new Rect(0, 0, thumbnail.width, thumbnail.height), new Vector2(0.5f, 0.5f)), offset);
             
         }
+        (content.transform as RectTransform).sizeDelta = new Vector2(i * 500, 400);
     }
 
     public void CreateFurnitureButton(GameObject furniture, Sprite thumbnail, float offset)
     {
         GameObject button = Instantiate(furnitureButtonPrefab);
-        button.transform.SetParent(emptyChild, false);
+        button.transform.SetParent(content, false);
         button.GetComponent<FurnitureButton>().furniture = furniture;
         Image image = button.transform.Find("Thumbnail").GetComponent<Image>();
         image.sprite = thumbnail;
